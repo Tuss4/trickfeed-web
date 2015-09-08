288edf2a4ceb17fbc6c20034f3b0a08f47658d81
Tuss4 <tuss4dbfn@gmail.com>
2015-09-08 09:48:01 -0500

    W00t success

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,32 @@
-// var VideoList = React.createClass({
-//     getInitialState: function() {
-//         return {data: []};
-//     },
-//     componentDidMount: function() {
-//         $.get(this.props.url, function(result) {
-//             console.log("The Results", result)
-//             var videos = result.results;
-//             if (this.isMounted()) {
-//                 this.setState({
-//                     data: videos
-//                 });
-//             }
-//         }.bind(this));
-
-//     },
-//     render: function() {
-//         return (
-//             <div className="videoList">
-//                 {this.state.data}
-//             </div>
-//         );
-//     }
-// });
-// React.render(
-//     <VideoList url="http://localhost:5000/v1/videos" />,
-//     document.getElementById('content')
-// );
-
-var url = "http://localhost:5000/v1/videos";
-$.ajax({
-    url: url,
-    type: 'GET',
-    success: function(data) {
-        console.log(data);
+var VideoList = React.createClass({
+    getInitialState: function() {
+        return {data: []};
     },
-    error: function(xhr, status, err) {
-        console.log("The url", url);
-        console.log(xhr.statusCode, status, err);
+    componentDidMount: function() {
+        $.get(this.props.url, function(result) {
+            console.log("The Results", result)
+            var videos = result.results;
+            if (this.isMounted()) {
+                this.setState({
+                    data: videos
+                });
+            }
+        }.bind(this));
+
+    },
+    render: function() {
+        return (
+            <div className="videoList">
+            <ul>
+                {this.state.data.map(function(video) {
+                    return <li><a href={video.video_url}>{video.title}</a></li>;
+                })}
+            </ul>
+            </div>
+        );
     }
 });
+React.render(
+    <VideoList url="http://localhost:5000/v1/videos" />,
+    document.getElementById('content')
+);
